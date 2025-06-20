edda5a950761b27c48f624adc7e325858321f322
Rudresh Jha <kumarrudresh5@gmail.com>
2025-06-20 14:17:35 +0530

    test

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body } = require('express-validator');
-const AuthController = require('../controllers/authcontroller);
+const AuthController = require('../controllers/authcontroller');
 const { authenticateToken } = require('../middlewares/auth.js');
 
 const router = express.Router();
