ad0bcd5ac98118df43e2fc26175588f5f510d1ff
Rudresh Jha <kumarrudresh5@gmail.com>
2025-06-24 18:06:23 +0530

    t

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
-const authRoutes = require('/authRoutes.js');
+const authRoutes = require('./authRoutes.js');
 const productRoutes = require('./routes/productRoutes.js');
 const adminRoutes = require('./routes/adminRoutes.js');
 
