d9705f85499027dea002e173c7a65c569d7c7c7f
Rudresh Jha <kumarrudresh5@gmail.com>
2025-06-24 17:48:47 +0530

    all new changes done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body } = require('express-validator');
-const AuthController = require('../controllers/AuthController');
+const AuthController = require('./controllers/AuthController');
 const { authenticateToken } = require('../middlewares/auth.js');
 
 const router = express.Router();
